3486ade237ba3dbd50924a9e023a56994d73a634
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-26 14:26:41 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -16,3 +16,11 @@ def display_board(board)
   puts "-----------"
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
+
+def input_to_index(user_input)
+  user_input.to_i - 1
+end
+
+def move(board, index, character = "X")
+ board[index] = character
+end
