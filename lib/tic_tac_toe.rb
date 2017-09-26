ea2900970f92d64c1f6715b58982fa2e3ee9f7dd
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-26 14:31:22 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -21,14 +21,22 @@ def input_to_index(user_input)
   user_input.to_i - 1
 end
 
+def move(board, index, character)
+ board[index] = character
+end
+
+def position_taken? (board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+     false
+  else
+     true
+end
+end
+
 def valid_move? (board, index)
 if index.between?(0,8) && !position_taken?(board, index)
   true
 else
   false
 end
-end
-
-def move(board, index, character)
- board[index] = character
 end
