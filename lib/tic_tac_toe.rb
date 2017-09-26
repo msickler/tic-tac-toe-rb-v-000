79c62694e7648ef1874976a47795399219522997
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-26 15:01:24 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -116,12 +116,13 @@ end
 end
 
 def play(board)
-until over?(board)
-  turn(board)
+   until over?(board)
+     turn(board)
+   end
+ 
+   if won?(board)
+     puts "Congratulations #{winner(board)}!"
+   elsif draw?(board)
+     puts "Cat's Game!"
+   end
  end
-  if won?(board)
-    puts "Congratulations #{winner(board)}!"
-  else draw?(board)
-    puts "Cat's Game!"
-end
-end
